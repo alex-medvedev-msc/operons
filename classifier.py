f13a4c355722419fde309bcd38b1e296de9fa408
Дмитрий Ромашко <wintor20@gmail.com>
2018-01-25 21:06:19 +1000

    gbt clsf

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import svm
 from sklearn.metrics import log_loss
 
 
@@ -13,11 +14,21 @@ def main():
     path = "features.csv"
 
     X_train, X_test, y_train, y_test = load_features(path)
-    classifier = GradientBoostingClassifier()
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict_proba(X_test)[:, 1]
+    gbt_classifier = GradientBoostingClassifier()
+    gbt_classifier.fit(X_train, y_train)
+    y_pred = gbt_classifier.predict_proba(X_test)[:, 1]
     preds = pandas.DataFrame.from_dict({"pred": list(y_pred), "test": list(y_test)})
+    print(preds)
     print(log_loss(y_test, y_pred))
+
+    svm_classifier = svm.SVC(probability=True)
+    svm_classifier.fit(X_train, y_train)
+
+    y_pred = svm_classifier.predict_proba(X_test)[:, 1]
+    preds = pandas.DataFrame.from_dict({"pred": list(y_pred), "test": list(y_test)})
+    print(preds)
+    print(log_loss(y_test, y_pred))
+
 
 
 if __name__ == '__main__':
